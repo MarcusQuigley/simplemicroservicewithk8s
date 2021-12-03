173fd1bda8e185a46706eb77ca1300d44287ac61
Marcus Quigley <marcusquigley@gmail.com>
2021-12-03 07:30:51 -0500

    adding ocelot

---
+++ src/annoying.apigateway/Programv6.cs
@@ -0,0 +1,11 @@
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+// builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+// builder.Logging.AddConsole();
+// builder.Logging.AddDebug();
+
+// var app = builder.Build();
+
+// app.MapGet("/", () => "Hello World!");
+
+// app.Run();
---
+++ src/annoying.apigateway/Startup.cs
@@ -0,0 +1,46 @@
+using System;
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+using Microsoft.Extensions.Configuration;
+
+
+namespace annoying.apigateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            await app.UseOcelot();
+        }
+    }
+
+
+}
